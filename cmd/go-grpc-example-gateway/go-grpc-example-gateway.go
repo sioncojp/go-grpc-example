02e97e9e7acaec18470e3042e94bbd958321f91d
sioncojp <sion_cojp@yahoo.co.jp>
2017-09-20 20:07:45 +0900

    implement grpc-gateway

---
+++ cmd/go-grpc-example-gateway/go-grpc-example-gateway.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+
+	gw "github.com/sioncojp/go-grpc-example/proto/message"
+
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+var (
+	echoEndpoint = flag.String("echo_endpoint", "localhost:50051", "endpoint of YourService")
+)
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := gw.RegisterMessageHandlerFromEndpoint(ctx, mux, *echoEndpoint, opts)
+	if err != nil {
+		return err
+	}
+
+	return http.ListenAndServe(":8080", mux)
+}
+
+func main() {
+	flag.Parse()
+	defer glog.Flush()
+
+	if err := run(); err != nil {
+		glog.Fatal(err)
+	}
+}
